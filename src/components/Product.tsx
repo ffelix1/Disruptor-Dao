d98c14c8859350e1e278413978d20644315ccc88
Amy <47039767+ffelix1@users.noreply.github.com>
2022-10-05 20:48:49 -0600

    full

---
+++ src/components/Product.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import config from '../config/index.json';
+import Divider from './Divider';
+
+const Product = () => {
+  const { product } = config;
+  const [firstItem, secondItem] = product.items;
+
+  return (
+    <section className={`bg-background py-8`} id="product">
+      <div className={`container max-w-5xl mx-auto m-8`}>
+        <h1
+          className={`w-full my-2 text-5xl font-bold leading-tight text-center text-primary`}
+        >
+          {product.title.split(' ').map((word, index) => (
+            <span
+              key={index}
+              className={index % 2 ? 'text-primary' : 'text-border'}
+            >
+              {word}{' '}
+            </span>
+          ))}
+        </h1>
+        <Divider />
+        <div className={`flex flex-wrap`}>
+          <div className={`w-5/6 sm:w-1/2 p-6 mt-20`}>
+            <h3
+              className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
+            >
+              {firstItem?.title}
+            </h3>
+            <p className={`text-gray-600`}>{firstItem?.description}</p>
+          </div>
+          <div className={`w-full sm:w-1/2 p-6`}>
+            <img
+              className="h-6/6"
+              src={firstItem?.img}
+              alt={firstItem?.title}
+            />
+          </div>
+        </div>
+        <div className={`flex flex-wrap flex-col-reverse sm:flex-row`}>
+          <div className={`w-full sm:w-1/2 p-6`}>
+            <img
+              className="h-6/6"
+              src={secondItem?.img}
+              alt={secondItem?.title}
+            />
+          </div>
+          <div className={`w-full sm:w-1/2 p-6 mt-20`}>
+            <div className={`align-middle`}>
+              <h3
+                className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
+              >
+                {secondItem?.title}
+              </h3>
+              <div className={`col-lg-6 col-sm-6 col-xs-12`}>
+                <li><b>Cultural norms</b> that promote patriarchal dominance in finance, investing, entrepreneurship and careers.</li>
+                <li><b>Economic oppression</b> that has wrought emotional, educational, legal, medical, and housing havoc throughout our communities.</li>
+                <li><b>Negative narratives</b>, thoughts, and paradigms that shape our mindsets.</li>
+              </div>
+              <p className={`text-gray-600 mb-8`}>{secondItem?.description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Product;
